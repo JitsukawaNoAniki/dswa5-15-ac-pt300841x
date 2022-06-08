0429d8311cce6b355d87afd733702383551deee1
Victor Jitsukawa <victor.jitsukawa@aluno.ifsp.edu.br>
2022-06-08 12:54:56 -0300

    a

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var app = require('./config/express')();
 require('./config/passport')();
 const url = 'mongodb+srv://dswa5:dswa5@clusterdswa5.xmkyt.mongodb.net/?retryWrites=true&w=majority';
+const localUrl = 'mongodb://localhost/DSWA'
 require('./config/database.js')(url);
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express Server escutando na porta ' + app.get('port'));
